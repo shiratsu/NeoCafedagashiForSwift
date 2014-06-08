20fe09ac8a9eec4e1136187a9511369ebad8f166
平塚俊輔 <shun_hiratsuka2002@yahoo.co.jp>
2014-06-08 14:51:31 +0900

    現在地までとれた

--- NeoCafeSagashiForSwift/ViewController.swift
+++ NeoCafeSagashiForSwift/ViewController.swift
@@ -7,12 +7,14 @@
 //
 
 import UIKit
+import CoreLocation
 
-class ViewController: UIViewController,GMSMapViewDelegate {
+class ViewController: UIViewController,GMSMapViewDelegate,CLLocationManagerDelegate {
     
     
     var calloutview: SMCalloutView?
-    var lm: CLLocationManager?
+    var lm: CLLocationManager!
+    let defaultRadius = 300
     
     @IBOutlet var mapview : GMSMapView
     @IBOutlet var gadbnrview : GADBannerView
@@ -22,12 +24,52 @@ class ViewController: UIViewController,GMSMapViewDelegate {
         // Do any additional setup after loading the view, typically from a nib.
         mapview.myLocationEnabled = true
         mapview.delegate = self
+        
+        lm = CLLocationManager()
+        lm.delegate = self
+        lm.desiredAccuracy = kCLLocationAccuracyBest
+        lm.requestAlwaysAuthorization()
+        lm.distanceFilter = 300
+        lm.startUpdatingLocation()
+        //startLocation()
+        
+        
+        
     }
 
+    func startLocation(){
+        NSLog("aaaaaa")
+        lm.startUpdatingLocation()
+    }
+    
+    func stopLocation(){
+        
+    }
+    
+    
+    
+    func locationManager(manager: CLLocationManager!, didUpdateToLocation newLocation: CLLocation!, fromLocation oldLocation: CLLocation!){
+        NSLog("bbb")
+        var coordinate: CLLocationCoordinate2D = CLLocationCoordinate2D(latitude:newLocation.coordinate.latitude,longitude:newLocation.coordinate.longitude)
+        var now :GMSCameraPosition = GMSCameraPosition.cameraWithLatitude(coordinate.latitude,longitude:coordinate.longitude,zoom:17)
+        mapview.camera = now
+       
+        
+        
+    }
+    
+    func locationManager(manager: CLLocationManager!, didFailWithError error: NSError!){
+        NSLog("ccc")
+    }
+    
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    
 
 
 }
